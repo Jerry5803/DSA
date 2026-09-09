e9266569c2b3a6958c404a99fd93ffdc0ec4f686
Jerry5803 <yubrajbasnet2552@gmail.com>
2026-09-09 23:00:24 +0545

    Time: 0 ms (100.00%) | Memory: 15.3 MB (37.21%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -5,11 +5,12 @@ public:
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
+
             if (nums[mid] == target) {
                 return mid;
             }
 
-            else if (nums[left] <= nums[mid]) {
+           else if (nums[left] <= nums[mid]) {
                 if (nums[left] <= target && target <= nums[mid]) {
                     right = mid - 1;
                 }
@@ -17,6 +18,7 @@ public:
                     left = mid + 1;
                 }
             }
+
             else {
                 if (nums[mid] <= target && target <= nums[right]) {
                     left = mid + 1;
@@ -25,6 +27,7 @@ public:
                     right = mid - 1;
                 }
             }
+
         }
         return -1;
     }
