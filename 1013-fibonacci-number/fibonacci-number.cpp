aed5989d6a0a4aae9b450a21b77a9c3fa97c9420
Jerry5803 <yubrajbasnet2552@gmail.com>
2026-09-15 19:10:46 +0545

    Time: 3 ms (64.47%) | Memory: 8.2 MB (17.07%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        vector<int> fb(n + 1);
+
+        fb[0] = 0;
+        fb[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fb[i] = fb[i - 1] + fb[i - 2];
+        }
+        return fb[n];
+    }
+};
