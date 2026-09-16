da93b3bc6bccc01137bea144c68dd2aba9642dca
Jerry5803 <yubrajbasnet2552@gmail.com>
2026-09-16 19:19:16 +0545

    Time: 31 ms (27.36%) | Memory: 16.6 MB (64.17%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -4,26 +4,23 @@ public:
         if (i < 0 || j < 0 || i >= n || j >= m || vis[i][j] || grid[i][j] != '1') {
             return;
         }
-
-        vis[i][j] = true;
+        vis[i][j] = 1;
 
         dfs(i - 1, j, vis, grid, n, m);
-        dfs(i, j + 1, vis, grid, n, m);
         dfs(i + 1, j, vis, grid, n, m);
-        dfs(i, j - 1, vis, grid, n, m);
-
+        dfs(i , j - 1, vis, grid, n, m);
+        dfs(i , j + 1, vis, grid, n, m);
     }
 
     int numIslands(vector<vector<char>>& grid) {
         int islands = 0;
         int n = grid.size();
         int m = grid[0].size();
-
         vector<vector<bool>>vis(n, vector<bool>(m, false));
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1' && !vis[i][j]) {
+                if (!vis[i][j] && grid[i][j] == '1') {
                     dfs(i, j, vis, grid, n, m);
                     islands++;
                 }
