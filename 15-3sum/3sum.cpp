81dd01a99be06b1741260dc55d1013f6e35c19fd
Jerry5803 <yubrajbasnet2552@gmail.com>
2026-09-20 08:11:25 +0545

    Time: 47 ms (62.67%) | Memory: 29.3 MB (28.30%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -19,6 +19,7 @@ public:
                     ans.push_back({nums[i], nums[left], nums[right]});
                     left++;
                     right--;
+                    
                     while (left < right && nums[left] == nums[left - 1]) {
                         left++;
                     }
