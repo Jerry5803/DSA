e767a934a86ae37d969cc18a3527a897791418b0
Jerry5803 <yubrajbasnet2552@gmail.com>
2026-09-24 08:12:43 +0545

    Time: 0 ms (100.00%) | Memory: 23.7 MB (72.39%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -15,8 +15,10 @@ public:
         if (root == nullptr) {
             return 0;
         }
+
         int lh = height(root->left, diameter);
         int rh = height(root->right, diameter);
+
         diameter = max(diameter, lh + rh);
         return (1 + max(lh, rh));
     }
